44004fb48ad6e8b87ae24bc70b7dbcff1a6425c7
Caijie0102 <lincaijie0102@gmail.com>
2023-02-22 11:55:15 +0800

    0222

---
+++ WebApplication0222/Controllers/test1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication0222.Controllers
+{
+    public class test1Controller : Controller
+    {
+        // GET: test1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
